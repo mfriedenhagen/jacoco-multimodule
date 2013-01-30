c2dafa0bbcdae60b483b5501ef42a038bfe3a2cf
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-01-30 22:27:11 +0100

    Clean up tests.

--- jmm-core/src/main/java/com/github/mfriedenhagen/jmm/core/App.java
+++ jmm-core/src/main/java/com/github/mfriedenhagen/jmm/core/App.java
@@ -1,13 +1,14 @@
 package com.github.mfriedenhagen.jmm.core;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public Date getDate() {
+        return new Date();
     }
 }
