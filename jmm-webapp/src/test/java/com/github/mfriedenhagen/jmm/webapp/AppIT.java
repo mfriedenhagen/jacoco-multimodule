eee3b7a63309c607d260e71c7cb1f730ed582d72
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-02-04 21:09:30 +0100

    Add first IT

--- jmm-webapp/src/test/java/com/github/mfriedenhagen/jmm/webapp/AppIT.java
+++ jmm-webapp/src/test/java/com/github/mfriedenhagen/jmm/webapp/AppIT.java
@@ -1,5 +1,13 @@
 package com.github.mfriedenhagen.jmm.webapp;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.ByteStreams;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.servlet.ServletException;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.junit.After;
@@ -15,12 +23,13 @@ public class AppIT {
     final String workingDir = System.getProperty("java.io.tmpdir");
 
     @Before
-    public void instantiateTomcatInstance() throws LifecycleException {
+    public void instantiateTomcatInstance() throws LifecycleException, ServletException {
         tomcat.setPort(0);
         tomcat.setBaseDir(workingDir);
         tomcat.getHost().setAppBase(workingDir);
         tomcat.getHost().setAutoDeploy(true);
         tomcat.getHost().setDeployOnStartup(true);
+        tomcat.addWebapp("/", new File("target/jmm-webapp").getAbsolutePath());
         tomcat.start();
     }
 
@@ -31,8 +40,16 @@ public class AppIT {
     }
 
     @Test
-    public void justATest() {
+    public void justATest() throws MalformedURLException, IOException {
         final int port = tomcat.getConnector().getLocalPort();
-        System.err.println(port);
+        final URL url = new URL("http://localhost:" + port + "/index.jsp");
+        final InputStream index = url.openStream();
+        try {
+            byte[] bodyAsBytes = ByteStreams.toByteArray(index);
+            final String bodyAsString = new String(bodyAsBytes, Charsets.UTF_8);
+            System.err.println(bodyAsString);
+        } finally {
+            index.close();
+        }
     }
 }
