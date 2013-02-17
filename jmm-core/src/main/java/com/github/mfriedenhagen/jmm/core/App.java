db4a7b5b9bb38ff352a8ec5d231f6029f476cde5
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-02-17 20:57:33 +0100

    Checkstyle

--- jmm-core/src/main/java/com/github/mfriedenhagen/jmm/core/App.java
+++ jmm-core/src/main/java/com/github/mfriedenhagen/jmm/core/App.java
@@ -4,10 +4,14 @@ import java.util.Date;
 
 /**
  * Hello world!
- *
+ * @author Mirko Friedenhagen
  */
 public class App {
 
+    /**
+     * Return date.
+     * @return new Date.
+     */
     public Date getDate() {
         return new Date();
     }
